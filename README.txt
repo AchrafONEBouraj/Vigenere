c58a6f1ca262c3da76cfb5a74cb25e73b0779108
Deus Kane <github.qxzkjp@spamgourmet.com>
2013-01-27 22:23:38 +0000

    Added support for command line arguments
    Cleaned up code a little bit as well, and added copyright notice.

--- README.txt
+++ README.txt
@@ -1,5 +1,14 @@
 Vigenere: a simple cipher implementation in C++
 
+Now supports command line arguments!
+
+-e or -d to specify encryption/decryption
+
+-if to specify input file (will append .txt if not present)
+-of to specity output file (will append .txt if not present)
+
+-key to specify key (alphabetic charecters only)
+
 This code compiles on the x64 VC2010 compiler, other compilers not tested.
 
 Code::Blocks 12 project files included, with targets for x86 and x64 VC2010, debug and release.
--- main.cpp
+++ main.cpp
@@ -26,20 +26,44 @@
 
 void encryptInit(std::string& pfile, std::string& cfile);
 void decryptInit(std::string& pfile, std::string& cfile);
+void genInit(const std::string& label, std::string& file);
+void doTheseAwfulThings(const std::string ifile, const std::string ofile, Encoder process, std::string (Encoder::*currentFunc)(std::string));
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::string ifile;
-    std::string ofile;
-    std::string key;
-    std::string flg;
+    std::cout << "Vigenere v0.5, copyright 2013 Deus Kane" << std::endl << "Distributed under the terms of the GNU GPL V3 (or later)" << std::endl;
+    #ifdef _DEBUG
+    std::cout << "argc=" << argc << std::endl;
+    for(int i=0;i<argc;++i){
+    std::cout << "argv[" << i << "]=" << argv[i] << std::endl;}
+    #endif
+
+//begin processing command line arguments
+    std::string flg="";
+    std::string arg;
+    std::string ifile="";
+    std::string ofile="";
+    std::string key="";
+    for(int i=0;i<argc;++i){
+        arg=argv[i];
+        if(arg[0]=='-'){
+            if(arg=="-e"){flg="e";}
+            if(arg=="-d"){flg="d";}
+            if(i<argc-1){
+                if(arg=="-if"){ifile=argv[i+1];++i;}
+                if(arg=="-of"){ofile=argv[i+1];++i;}
+                if(arg=="-key"){key=argv[i+1];++i;}}}}
+//end processing command line arguments
+
     Encoder testing;
     std::string (Encoder::*currentFunc)(std::string);
 
-    do{
-        std::cout << "Encrypt or decrypt? ";
-        std::getline(std::cin, flg);
-    }while(flg.size()==0 || (flg[0]!='d' && flg[0]!='D' && flg[0]!='e' && flg[0]!='E'));
+    if(flg==""){
+        do{
+            std::cout << "Encrypt or decrypt? ";
+            std::getline(std::cin, flg);
+            if(flg=="x"){return 0;}
+        }while(flg.size()==0 || (flg[0]!='d' && flg[0]!='D' && flg[0]!='e' && flg[0]!='E'));}
 
     if(flg[0]=='d'||flg[0]=='D'){
         decryptInit(ifile,ofile);
@@ -48,40 +72,20 @@ int main()
         encryptInit(ifile,ofile);
         currentFunc=&Encoder::encrypt;}
 
-    do{
-        std::cout << "Enter key (leave blank for CorrectHorseBatteryStaple): ";
-        std::getline(std::cin, key);
-        if(key == ""){key="CorrectHorseBatteryStaple";}
-        testing.setKey(key);
-    }while(testing.keySize()<=0);
+    if(key==""){
+        do{
+            std::cout << "Enter key (leave blank for CorrectHorseBatteryStaple): ";
+            std::getline(std::cin, key);
+            if(key == ""){key="CorrectHorseBatteryStaple";}
+            testing.setKey(key);
+        }while(testing.keySize()<=0);
+        }else{
+            testing.setKey(key);}
 
-    std::ifstream infile (ifile);
-    std::ofstream outfile (ofile);
-
-    std::string buffer;
-    std::string ebuffer;
-
-    if (infile.is_open()){
-        while ( infile.good() ){
-            getline (infile,buffer);
-            ebuffer=(testing.*currentFunc)(buffer);
-            std::cout << ebuffer << std::endl;
-            outfile << ebuffer;
-            if(infile.good()){ outfile << "\n";}}
-        infile.close();
-        outfile.close();
-    }else{
-        std::cout << "File not found!" << std::endl;}
+    doTheseAwfulThings(ifile, ofile, testing, currentFunc);
 
     return 0;
 }
-
-void genInit(const std::string& label, std::string& file){
-    std::cout << "Enter " << label << " file name (leave blank for default): ";
-    std::getline(std::cin, file);
-    if(file == ""){file=label+".txt";}
-    if(file.substr(file.length()-4,4)!=".txt"){
-        file+=".txt";}}
 
 void encryptInit(std::string& pfile, std::string& cfile){
     genInit("plaintext",pfile);
@@ -92,3 +96,34 @@ void decryptInit(std::string& cfile, std::string& pfile){
     genInit("ciphertext",cfile);
     genInit("plaintext",pfile);
 }
+
+void genInit(const std::string& label, std::string& file){
+    if(file==""){
+        std::cout << "Enter " << label << " file name (leave blank for default): ";
+        std::getline(std::cin, file);
+        if(file == ""){file=label+".txt";}}
+    if(file.substr(file.length()-4,4)!=".txt"){
+        file+=".txt";}}
+
+void doTheseAwfulThings(const std::string ifile, const std::string ofile, Encoder process, std::string (Encoder::*currentFunc)(std::string)){
+    std::ifstream infile (ifile);
+    std::ofstream outfile (ofile);
+    std::string buffer;
+
+    if (infile.is_open() && outfile.is_open()){
+        while ( infile.good() ){
+            getline (infile,buffer);
+            buffer=(process.*currentFunc)(buffer);
+            #ifdef _DEBUG
+            std::cout << buffer << std::endl;
+            #endif
+            outfile << buffer;
+            if(infile.good()){ outfile << "\n";}}
+        infile.close();
+        outfile.close();
+        #ifdef _DEBUG
+        std::cout << std::endl;
+        #endif
+        std::cout << "Process completed successfully." << std::endl;
+    }else{
+        std::cout << "File not found!" << std::endl;}}
